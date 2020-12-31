e5a2ab2136c02b2ced9e85cdfc2ee8b6e155d9ce
mauricelambert <50479118+mauricelambert@users.noreply.github.com>
2020-12-31 18:00:28 +0100

    Update SimpleTelnetMail.py

--- SimpleTelnetMail/SimpleTelnetMail.py
+++ SimpleTelnetMail/SimpleTelnetMail.py
@@ -1,7 +1,28 @@
-from telnetlib import Telnet
-from ssl import _create_stdlib_context
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###################
+#    This package implement a simple SMTP client with Telnet.
+#    Copyright (C) 2020  Maurice Lambert
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+###################
 
 """ This file implement the TelnetMail class. """
+
+from telnetlib import Telnet
+from ssl import _create_stdlib_context
 
 # Datetime Format : Sat, 25 Dec 2021 05:35:23 -0000
 
